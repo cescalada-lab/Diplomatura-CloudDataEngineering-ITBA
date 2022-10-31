50310a94c68542795bceed833fbbbe48b7ab409e
cescalada-lab <69772417+cescalada-lab@users.noreply.github.com>
2022-10-31 05:04:14 -0300

    Add files via upload

---
+++ 1_Foundations/Trabajo_Practico_Final_Foundations/Punto_4/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER"  <<-EOSQL
+
+    DROP DATABASE IF EXISTS amir_deals;
+
+    CREATE DATABASE amir_deals;
+
+	\c amir_deals ;
+    
+    DROP TABLE IF EXISTS amir_deals;
+
+    CREATE TABLE IF NOT EXISTS amir_deals(
+	    id		    INTEGER,
+	    product     TEXT,
+	    client      TEXT,
+	    status      TEXT,
+	    amount      DECIMAL(8,2),
+	    num_users   INTEGER);
+
+EOSQL
---
+++ 1_Foundations/Trabajo_Practico_Final_Foundations/Punto_4/populate-db/populate.py
@@ -0,0 +1,19 @@
+#populate.py - Punto-4
+
+import psycopg2
+import time
+
+print("Ahora comenzará a ejecutarse el script para rellenar las columnas de la tabla amir_deals")
+
+time.sleep(100)
+conn = psycopg2.connect("host=app-postgres-db dbname=amir_deals user=postgres password=postgres")
+cur = conn.cursor()
+with open('/populate-db/amir_deals.csv', 'r') as f:
+    # Notitce that we don't need the `csv` module.
+    next(f) # Skip the header row.
+    cur.copy_from(f, 'amir_deals', sep=',')
+
+conn.commit()
+time.sleep(100)
+
+print("Ya terminó de ejecutarse el script con éxito!!!...")
